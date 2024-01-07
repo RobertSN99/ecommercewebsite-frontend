37604763d38aef45b701fec177f8e6886f89f26c
RobertSN99 <robertsrn15@gmail.com>
2024-01-08 01:32:31 +0200

    footer buttons open to new tab

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,11 +13,11 @@ const Footer = () => {
                 <p className="text-white text-sm tracking-wide">© TimeHub.com</p>
                 <img className='w-56 sm:w-32' src={payment} alt="payment" />
                 <div className='flex gap-5 text-lg sm:text-sm text-gray-400'>
-                    <a href='https://github.com/RobertSN99/'><ImGithub className='hover:text-white duration-300 cursor-pointer'/></a>
-                    <a href='https://www.linkedin.com/in/robert-spinoiu-06a9b120a/'><FaLinkedin className='hover:text-white duration-300 cursor-pointer'/></a>
-                    <a href=''><FaFacebookF className='hover:text-white duration-300 cursor-pointer'/></a>
-                    <a href=''><FaTwitter className='hover:text-white duration-300 cursor-pointer'/></a>
-                    <a href='https://instagram.com/srobertn99'><FaInstagram className='hover:text-white duration-300 cursor-pointer'/></a>
+                    <a href='https://github.com/RobertSN99/' target='_blank'><ImGithub className='hover:text-white duration-300 cursor-pointer'/></a>
+                    <a href='https://www.linkedin.com/in/robert-spinoiu-06a9b120a/' target='_blank'><FaLinkedin className='hover:text-white duration-300 cursor-pointer'/></a>
+                    <a href='' target='_blank'><FaFacebookF className='hover:text-white duration-300 cursor-pointer'/></a>
+                    <a href='' target='_blank'><FaTwitter className='hover:text-white duration-300 cursor-pointer'/></a>
+                    <a href='https://instagram.com/srobertn99' target='_blank'><FaInstagram className='hover:text-white duration-300 cursor-pointer'/></a>
                 </div>
             </div>
             <div className='text-base flex flex-col gap-2'>
